8950225ffbf22111c837f62e04e8f6637e5b1910
Sigalov <sigalov.oleg@gmail.com>
2017-11-01 17:38:13 +0200

    improve logger

--- screens/login.py
+++ screens/login.py
@@ -1,10 +1,14 @@
 import sys
+
 from appium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+
 from android.screens.login_android import LoginAndroid
 from ios.screens.login_ios import LoginIos
 from lib import logger
 from screens.base import Base
+from screens.general import General
+from screens.home import Home
 
 
 class Login(Base):
@@ -27,49 +31,74 @@ class Login(Base):
             platform = None
             #TODO PRINT ERROR
         
-        
+        # For internal use (Reusable flow methods in this class)
+        self.home     = Home(platform, driver) 
+        self.general  = General(platform, driver)
     #############################################################################
     ############################# Click (tap) methods ###########################    
     # Click 'LOGIN'  
     def click_login_button(self):
         if self.click(self.login.login_button) == True:
-            logger.debug('Function: "' + sys._getframe().f_code.co_name + '" - Clicked on login button')
+            logger.debugLog('Function: "' + sys._getframe().f_code.co_name + '" - Clicked on login button')
             return True
         else:
-            logger.info('WARNING: Function: "' + sys._getframe().f_code.co_name + '" - Failed to click on login button')
+            logger.infoLog('WARNING: Function: "' + sys._getframe().f_code.co_name + '" - Failed to click on login button')
             raise Exception('Failed to click on login button')
         
     # Click 'Sign in'    
     def click_signin_button(self):
         if self.click(self.login.signin_button) == True:
-            logger.debug('Function: "' + sys._getframe().f_code.co_name + '" - Clicked on signin button')
+            logger.debugLog('Function: "' + sys._getframe().f_code.co_name + '" - Clicked on signin button')
             return True
         else:
-            logger.info('WARNING: Function: "' + sys._getframe().f_code.co_name + '" - Failed to click on signin button')
+            logger.infoLog('WARNING: Function: "' + sys._getframe().f_code.co_name + '" - Failed to click on signin button')
             return False        
         
     def click_coordinates_login_frame(self, x, y):
         if self.click_with_offset(self.login.login_frame, x, y) == True:
-            logger.debug('Function: "' + sys._getframe().f_code.co_name + '" - Clicked with coordinates on login frame')
+            logger.debugLog('Function: "' + sys._getframe().f_code.co_name + '" - Clicked with coordinates on login frame')
             return True
         else:
-            logger.info('WARNING: Function: "' + sys._getframe().f_code.co_name + '" - Failed to click with coordinates on login frame: x,y:' + str(x) + ',' + str(y))
-            return False         
+            logger.infoLog('WARNING: Function: "' + sys._getframe().f_code.co_name + '" - Failed to click with coordinates on login frame: x,y:' + str(x) + ',' + str(y))
+            return False
+        
+    #############################################################################
+    ############################# Wait for methods ##############################          
+    def wait_for_logged_in_background(self, timeout):
+        if self.wait_visible(self.login.logged_in_background, timeout) != None:
+            logger.debugLog('Function: "' + sys._getframe().f_code.co_name + '" - Wait for logged_in_background')
+            return True
+        else:
+            logger.infoLog('WARNING: Function: "' + sys._getframe().f_code.co_name + '" - Element logged_in_background doesn\'t appear after ' + str(timeout))
+            return False   
+               
     
     #############################################################################
     ############################# Type text methods #############################
     def type_login_username(self, text):
         if self.send_keys(self.login.text_login_username, text)  == True:
-            logger.debug('Function: "' + sys._getframe().f_code.co_name + '" - Type in login username filed: "' + text + '"')
+            logger.debugLog('Function: "' + sys._getframe().f_code.co_name + '" - Type in login username filed: "' + text + '"')
             return True
         else:
-            logger.info('WARNING: Function: "' + sys._getframe().f_code.co_name + '" - Failed to type in login username filed: "' + text + '"')
+            logger.infoLog('WARNING: Function: "' + sys._getframe().f_code.co_name + '" - Failed to type in login username filed: "' + text + '"')
             return False          
         
     def type_login_password(self, text):
         if self.send_keys(self.login.text_login_password, text)  == True:
-            logger.debug('Function: "' + sys._getframe().f_code.co_name + '" - Type in login password filed: "' + text + '"')
+            logger.debugLog('Function: "' + sys._getframe().f_code.co_name + '" - Type in login password filed: "' + text + '"')
             return True
         else:
-            logger.info('WARNING: Function: "' + sys._getframe().f_code.co_name + '" - Failed to type in login password filed: "' + text + '"')
+            logger.infoLog('WARNING: Function: "' + sys._getframe().f_code.co_name + '" - Failed to type in login password filed: "' + text + '"')
             return False
+        
+    #############################################################################
+    ############################# Reusable flow methods #########################    
+    def login_with_credentials(self, username, password):
+        self.home.click_user_image_view()
+        self.click_login_button()
+        self.type_login_username(username)
+        self.type_login_password(password)
+        self.general.hide_keyboard()
+        self.click_signin_button()
+        self.wait_for_logged_in_background(15)
+        logger.infoLog("Logged in successfully with '" + "'" + username + "'@'" + password + "'")
